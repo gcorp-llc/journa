eff8cb39f294fea3724aed31a7cd67ff1bc81542
gcorp-llc <hosseinradvictor@gmail.com>
2025-09-30 12:16:04 +0330

    insert localisation for errors page

---
+++ resources/lang/en/errors.php
@@ -0,0 +1,30 @@
+<?php
+
+// resources/lang/en/errors.php
+
+return [
+    '403' => [
+        'title'   => 'Forbidden',
+        'message' => 'Sorry, you are forbidden from accessing this page. Perhaps you should return to the lobby.',
+    ],
+    '404' => [
+        'title'   => 'Page Not Found',
+        'message' => 'Uh oh! It looks like you are lost in this galaxy. The page you were looking for does not exist.',
+    ],
+    '419' => [
+        'title'   => 'Page Expired',
+        'message' => 'This page has been waiting for you for too long. Please refresh and try again.',
+    ],
+    '429' => [
+        'title'   => 'Too Many Requests',
+        'message' => 'You\'re moving a little too fast! Please wait a moment and then try again.',
+    ],
+    '500' => [
+        'title'   => 'Internal Server Error',
+        'message' => 'Something went wrong on our planet! Our technical team is investigating the incident.',
+    ],
+    '503' => [
+        'title'   => 'Service Unavailable',
+        'message' => 'We are performing some updates to make everything work better. We will be back soon!',
+    ],
+];
